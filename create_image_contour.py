a036d8edddf885516f3fdddc74f3c8d6f68f0d3d
Bradley Bossard <bradleybossard@gmail.com>
2015-01-13 09:50:45 -0800

    Create an image contour

---
+++ create_image_contour.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from pylab import *
+# read image to array
+im = array(Image.open('empire.jpg').convert('L'))
+# create a new figure
+figure()
+# don't use colors
+gray()
+# show contours with origin upper left corner
+contour(im, origin='image')
+axis('equal')
+axis('off')
+#show()
+savefig('empire_contour.jpg')
