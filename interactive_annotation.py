eba289aa5b7b483cb24994694d673d57488e7da3
Bradley Bossard <bradleybossard@gmail.com>
2015-01-13 10:03:29 -0800

    Adding interactive example

---
+++ interactive_annotation.py
@@ -0,0 +1,12 @@
+"""
+Example of pylab interactivity
+"""
+
+from PIL import Image
+from pylab import *
+im = array(Image.open('empire.jpg'))
+imshow(im)
+print 'Please click 3 points'
+x = ginput(3)
+print 'you clicked:',x
+show()
