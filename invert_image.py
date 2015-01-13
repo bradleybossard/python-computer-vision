fffa43689f0db0a1af7a681f05cf8dbe61199be5
Bradley Bossard <bradleybossard@gmail.com>
2015-01-13 10:11:31 -0800

    Adding example for inverting an image

---
+++ invert_image.py
@@ -0,0 +1,11 @@
+"""
+  Example for inverting an image.
+"""
+
+from PIL import Image
+from numpy import *
+# Read image into numpy array
+im = array(Image.open('empire.jpg').convert('L'))
+im2 = 255 - im #invert image
+pil_im = Image.fromarray(im2)
+pil_im.save('empire_inverted.jpg')
